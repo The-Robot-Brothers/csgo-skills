733bafacfe157864d03a5b27afeb9d80135c65ab
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-05-18 16:56:07 -0400

    upd SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,16 +5,14 @@ export function SideBar() {
     <div className="container">
       <nav className="sidebar">
         <span className="title">
-          CSGO <p>Skills</p>
+          CSGO<p>Skills</p>
         </span>
-        <div className="container-buttons">
-          <button className="button-map">Dust 2</button>
-          <button className="button-map">Inferno</button>
-          <button className="button-map">Mirage</button>
-          <button className="button-map">Train</button>
-          <button className="button-map">Overpass</button>
-          <button className="button-map">Nuke</button>
-        </div>
+        <button className="button-map selected">Dust 2</button>
+        <button className="button-map">Inferno</button>
+        <button className="button-map">Mirage</button>
+        <button className="button-map">Train</button>
+        <button className="button-map">Overpass</button>
+        <button className="button-map">Nuke</button>
       </nav>
     </div>
   )
