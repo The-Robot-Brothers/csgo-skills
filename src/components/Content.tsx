398aab8e572cdd6f613e98f2df3c279c21b90a37
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-05-26 12:53:05 -0400

    add bombs on content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -5,6 +5,12 @@ import '../styles/content.scss'
 import CtImg from '../assets/icon/ct.png'
 import TrImg from '../assets/icon/tr.png'
 
+import SelectTeamImg from '../assets/icon/select.svg'
+import FlashImg from '../assets/bombs/flash.svg'
+import GrenadeImg from '../assets/bombs/grenade.svg'
+import MolotovImg from '../assets/bombs/molotov.svg'
+import SmokeImg from '../assets/bombs/smoke.svg'
+
 export function Content() {
   const { map } = useMap()
 
@@ -18,6 +24,24 @@ export function Content() {
         </button>
         <button>
           <img src={CtImg} alt="Counter Terrorist" />
+        </button>
+      </div>
+
+      <div className="menu-bombs">
+        <button>
+          <img src={SelectTeamImg} alt="select team" />
+        </button>
+        <button>
+          <img src={FlashImg} alt="flash" />
+        </button>
+        <button>
+          <img src={GrenadeImg} alt="grenade" />
+        </button>
+        <button>
+          <img src={MolotovImg} alt="molotov" />
+        </button>
+        <button>
+          <img src={SmokeImg} alt="smoke" />
         </button>
       </div>
     </div>
